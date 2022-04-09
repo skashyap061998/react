178d4addaa80e4c7e6f2a0713cab880e7327acfd
Shubham Kashyap <skashyap061998@gmail.com>
2022-04-09 19:23:48 +0530

    asssn 1

---
+++ day1/assn1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Butt} from "./component/Prb1"
+
+function App() {
+  return (
+    <div className="App">
+      <Butt/>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ day1/assn1/src/component/Prb1.jsx
@@ -0,0 +1,25 @@
+const Butt =  ()=>{
+    return (
+        <>
+        <h1>Mobile Operating system</h1>
+    <ul>
+        <li>Android</li>
+        <li>Blueberry</li>
+        <li>Iphone</li>
+        <li>Window phone</li>
+    </ul>
+    
+    <h1>Mobile Manufacturers</h1>
+    <ul>
+        <li id="sam">Samsung</li>
+        <li id="htc">HTC</li>
+        <li id="app">Apple</li>
+        <li id="xi">Xiomi</li>
+    </ul>
+    </>
+    )
+    
+    }
+    
+    
+    export {Butt}
